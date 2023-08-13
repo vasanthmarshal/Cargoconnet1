4ede9c9128e67cf327a431fb8ec5d7b954133a91
VASANTH MARSHAL J <96982683+vasanthmarshal@users.noreply.github.com>
2023-08-13 20:37:32 +0530

    Update UserDetails.js

--- models/UserDetails.js
+++ models/UserDetails.js
@@ -4,8 +4,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 // Define the user schema
 const UserDetails = new mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: {type: String,required: true,unique: true},
+  email: { type: String, required: true },
+  phone: {type: String,required: true},
   password: { type: String, required: true }
 });
 
